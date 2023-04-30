4a608cb9ce96c5920e85911e426899ab733b6487
Umair Ali <87652686+ali-umair@users.noreply.github.com>
2023-04-30 03:37:50 -0400

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+//@ts-ignore
+import Input from './components/Input.jsx'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-purple-700'>Car Sharing App</h1>
+      <Input />
+    </>
+  )
+}
+
+export default App
