aa0eeaebf82a7ab441f57e03267cc090d50a4806
Umair Ali <87652686+ali-umair@users.noreply.github.com>
2023-05-03 11:18:09 +0000

    From design and payload

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,19 @@ import './App.css';
 import Input from './components/Input.jsx'
 
 function App() {
+  const submitForm = (e: any) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const payload = Object.fromEntries(formData);
+    console.log(payload);
+  }
   return (
-    <>
-      <h1 className='text-purple-700'>Car Sharing App</h1>
-      <Input />
-    </>
+    <form className='bg-gray-800 w-1/3 m-auto mt-10 p-10 rounded-md flex flex-col gap-6' onSubmit={submitForm}>
+      <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>Sign in to your account</h1>
+      <Input type="email" name="email" labelText="Email" placeholder="Your Email" />
+      <Input type="password" name="password" labelText="Password" placeholder="Your Password" />
+      <button type="submit" className="w-full mt-5 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
+    </form>
   )
 }
 
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-export default function Input() {
+export default function Input(props) {
     return (
-        <>
-            <div>
-                <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
-                <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
-            </div>
-        </>
+        <div className='w-full'>
+            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-slate-200">{props.labelText}</label>
+            <input type={props.type} name={props.name} placeholder={props.placeholder} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+        </div>
     )
 }
