3845745b5b7f28f817b00cfc3b230522f634d01e
Umair Ali <87652686+ali-umair@users.noreply.github.com>
2023-05-15 08:40:07 +0000

    Popup form design complete - MVP

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,16 +1,14 @@
-
 export default function Dropdown(props: any) {
     return (
-        <div className="w-full xl:w-[45%] lg:w-[40%]">
-            <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-slate-200">dfsdfsdfsd</label>
-            <select name="pets" id="pet-select" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:outline-none w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:hover:border-blue-500">
-                <option value="">--Please choose an option--</option>
-                <option value="dog">Dog</option>
-                <option value="cat">Cat</option>
-                <option value="hamster">Hamster</option>
-                <option value="parrot">Parrot</option>
-                <option value="spider">Spider</option>
-                <option value="goldfish">Goldfish</option>
+        <div className="w-full xl:w-[45%] lg:w-[40%] mb-7 sm:mb-0">
+            <label htmlFor={props.labelFor} className="block mb-2 text-sm font-medium text-slate-200">{props.labelText}</label>
+            <select name="pets" id="pet-select" className="border border-gray-300 text-sm rounded-lg focus:outline-none w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 hover:border-blue-500">
+                <option value="">{props.placeholder}</option>
+                {props.options.map((option: any) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
             </select>
         </div>
     )
--- src/components/PopupForm.tsx
+++ src/components/PopupForm.tsx
@@ -2,21 +2,22 @@ import Dropdown from './Dropdown'
 import TextField from './TextField'
 
 export default function PopupForm() {
+  let carTypes: Array<string> = ["Small Hatchback (Mehran, Alto etc.)", "Hatchback (Cultus, Passo etc.)", "Sedan (Corolla, Civic etc.)", "SUV (Sportage, BRV etc.)" , "Other"];
   return (
     <form action="#" className="mt-5 flex flex-wrap gap-5 justify-center">
-      <Dropdown />
-      <TextField labelText="To" type="dropdown" name="name" placeholder="placeholder" />
-      <TextField labelText="Date" type="date" name="name" placeholder="placeholder" />
-      <TextField labelText="Time" type="time" name="name" placeholder="placeholder" />
-      <TextField labelText="Fare (Rs.)" type="number" name="name" placeholder="placeholder" />
-      <TextField labelText="Car Type" type="dropdown" name="name" placeholder="placeholder" />
-      <TextField labelText="Air Conditioning (AC)" type="dropdown" name="name" placeholder="placeholder" />
-      <div className="w-full xl:w-[45%] lg:w-[40%]">
-            <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-slate-200">Comments</label>
-            <textarea rows={1} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
+      <TextField labelText="Pickup" type="text" name="pickup_location" placeholder="St # 01, H # 01, A-Block, B-17" />
+      <TextField labelText="Dropoff" type="text" name="dropoff_location" placeholder="Zero Point ISB" />
+      <TextField labelText="Date" type="date" name="date" />
+      <TextField labelText="Time" type="time" name="time"/>
+      <TextField labelText="Fare (Rs.)" type="number" name="fare" placeholder="Enter Fare in Rupees" />
+      <Dropdown labelFor="carType" labelText="Car Type" placeholder="-- Choose a car type --" options={carTypes}/>
+      <Dropdown labelFor="AC" labelText="Air Conditioning (AC)" placeholder="-- Choose an option --" options={["Yes", "No"]}/>
+      <div className="w-full xl:w-[45%] lg:w-[40%] mb-3 sm:mb-0">
+        <label htmlFor="comments" className="block mb-2 text-sm font-medium text-slate-200">Comments</label>
+        <textarea rows={1} className="bg-gray-50 border text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:border-blue-500"></textarea>
       </div>
       <div className='w-full text-center'>
-        <button type="submit" className="w-1/2 mt-5 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button type="submit" className="w-1/2 mt-5 hover:bg-blue-700 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 focus:ring-blue-800">
           Sign in
         </button>
       </div>
