1ef3f14332c6f7ec7bca0ba9cd48756a4637fc37
Umair Ali <87652686+ali-umair@users.noreply.github.com>
2023-05-16 11:51:24 +0000

    Visual changes

--- src/components/DataField.tsx
+++ src/components/DataField.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function DataField(props: any) {
   return (
-    <div className='flex items-center w-80 mt-10 sm:mt-0 font-sans'>
+    <div className='flex items-center gap-7 w-80 mt-12 sm:mt-0 font-sans bg-gray-900 rounded-lg px-3 py-5 shadow-2xl shadow-gray-900'>
       {/* <p className="h-fit mr-7 p-2 rounded-lg" style={{backgroundColor: `${props.bgColor}`}}>{props.dataLabel}: </p> */}
-      <p className="h-fit sm:w-24 mr-7 p-2 bg-blue-600 text-center rounded-lg">{props.dataLabel}: </p>
+      <p className="h-fit sm:w-24 p-2 text-center text-blue-300 rounded-lg">{props.dataLabel}: </p>
       <p className="">{props.dataText}</p>
     </div>
   )
